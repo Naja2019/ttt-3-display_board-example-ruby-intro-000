de321b7b8d641a82114435013aef8c29168017ec
Naja Aldefri <naja.aldefri@gmail.com>
2019-01-06 03:22:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  phrase "Tic Tac Toe Board"
 puts "   |   |   "
 puts "-----------
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
 end
-puts display_board
